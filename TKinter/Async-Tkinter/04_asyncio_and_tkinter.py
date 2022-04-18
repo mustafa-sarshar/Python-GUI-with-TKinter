aebca429b7f33c7e2e8dc53b745fd1492b4cbd33
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2022-04-18 21:01:16 +0200

    update

--- TKinter/Async-Tkinter/04_asyncio_and_tkinter.py
+++ TKinter/Async-Tkinter/04_asyncio_and_tkinter.py
@@ -12,6 +12,14 @@ def update_gui(e):
     current_time = datetime.now().strftime("%H:%M:%S.%f - %A, %d %B %Y")
     var_time.set(current_time)
 
+async def update_gui_automatically():
+    while True:
+        current_time = datetime.now().strftime("%H:%M:%S.%f - %A, %d %B %Y")
+        var_time.set(current_time)
+        print(current_time)
+        await asyncio.sleep(1)
+
+
 window = tk.Tk()
 window.wm_title("Asyncio in Tkinter")
 window.geometry("300x300")
@@ -24,6 +32,15 @@ lbl_time = ttk.Label(master=window, textvariable=var_time)
 lbl_time.pack(side="top")
 lbl_time.bind("<Button>", update_gui)
 
+async def gui():
+    window.mainloop(1)
+    print("GUI")
+    await asyncio.sleep(1)
+    
+async def wait_list():
+    await asyncio.wait([gui(), update_gui_automatically()])
 
 if __name__ == "__main__":
-    window.mainloop()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(wait_list())
+    loop.close()

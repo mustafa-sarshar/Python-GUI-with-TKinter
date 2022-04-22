b874f83a71f39edbd2f63103ba877fb6560a556c
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2022-04-22 20:55:44 +0200

    update

---
+++ Kivy/Basics/12-Switch/main.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty, BooleanProperty
+
+class WidgetsExample(GridLayout):
+    n_clicked = 0
+    count_enabled = BooleanProperty(False)
+    my_text = StringProperty("Hello!")
+
+    def on_button_click(self):
+        if self.count_enabled:
+            self.n_clicked += 1
+            self.my_text = f"Clicked for {self.n_clicked} time(s)!"
+
+    def on_switch_active(self, widget):
+        if widget.active:
+            self.count_enabled = True
+        else:
+            self.count_enabled = False
+        message = f"Switch is {widget.active}"
+        print(message)
+
+class TheLabApp(App):
+    pass
+
+if __name__ == "__main__":
+    TheLabApp().run()

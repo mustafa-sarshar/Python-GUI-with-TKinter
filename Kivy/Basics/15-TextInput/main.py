2f68bc59e0e468fded2ffb1aafed78bb11446387
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2022-04-22 21:44:14 +0200

    update

---
+++ Kivy/Basics/15-TextInput/main.py
@@ -0,0 +1,13 @@
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class WidgetsExample(GridLayout):
+    pass
+
+class TheLabApp(App):
+    pass
+
+if __name__ == "__main__":
+    TheLabApp().run()
